266dcf5d0cb753a701139b272675d00810fe55fe
Ольга <o.kesterson@yandex.ru>
2023-04-21 19:21:19 +0300

    init

---
+++ main.py
@@ -0,0 +1,30 @@
+from main2 import upper, lower
+
+
+def sum_arg(a, b):
+    return a + b
+
+
+print(sum_arg(a=1, b=2))
+
+print(sum([1, 7]))
+
+print('A' * 5)
+
+a: dict = {
+    'a': 5,
+    'b': 3
+}
+b: list = [1, 2, 3]
+c: tuple = (1, 2, 3)
+d: set = set()
+
+names = ['Тимур', 'Стас']
+
+names.append('Саша')
+names.append('Саша')
+
+print(set(names))
+
+print(upper('abc'))
+print(lower('aBc'))
---
+++ main2.py
@@ -0,0 +1,6 @@
+def upper(value: str):
+    return value.upper()
+
+
+def lower(value: str):
+    return value.lower()
